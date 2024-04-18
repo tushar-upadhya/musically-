f711fc1c257c66f957df465e7b3c8388aac6e3ab
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-04-18 16:52:34 +0530

    .

---
+++ src/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: " tushar Music School",
+  description: "Music School By tushar Singh",
+  icons: {
+    icon: "favicon.ico",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <div className="relative w-full flex items-center justify-center">
+          <Navbar />
+        </div>
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
